6a0668df91437b2f698b2c5713ba187c73fef9b3
Felipe Alves <mrfelipealves3@gmail.com>
2018-06-06 17:07:15 -0300

    Finalizando client

---
+++ Desafio-Client/src/br/com/bean/ContaBean.java
@@ -0,0 +1,71 @@
+package br.com.bean;
+
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
+
+import br.com.service.ContaService;
+import br.com.to.Conta;
+
+@ManagedBean
+public class ContaBean {
+
+	private Conta conta;
+	private ContaService service;
+	
+	@PostConstruct
+	private void init() {
+		conta = new Conta();
+		service = new ContaService();
+	}
+	
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+	
+	public Conta getConta() {
+		return conta;
+	}
+	
+	public List<Conta> getContas(){
+		try {
+			return service.listar();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public String salvar() {
+		FacesMessage msg;
+		try {
+			if(conta.getId() == 0) {
+				service.cadastrar(conta);
+				msg = new FacesMessage("Conta cadastrada com sucesso!!");
+			}else {
+				service.atualizar(conta);
+				msg = new FacesMessage("Cadastro atualizado com sucesso!");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+			msg = new FacesMessage("Erro ao cadastrar/atualizar!");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		return "conta-lista?faces-redirect=true";
+		
+	}
+	
+	public String deletar(int codigo) {
+		FacesMessage msg;
+		try {
+			service.remover(codigo);
+			msg = new FacesMessage("Conta apagada");
+		}catch (Exception e) {
+			e.printStackTrace();
+			msg = new FacesMessage("Erro ao apagar conta");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		return "conta-lista?faces-redirect=true";
+	}
+}
--- Desafio-Client/src/br/com/service/ContaService.java
+++ Desafio-Client/src/br/com/service/ContaService.java
@@ -1,8 +1,9 @@
 package br.com.service;
 
-import org.apache.catalina.WebResource;
+import java.util.Arrays;
+import java.util.List;
 
-import com.sun.security.ntlm.Client;
+import javax.ws.rs.core.MediaType;
 
 import br.com.to.Conta;
 
@@ -21,4 +22,33 @@ public class ContaService {
 			throw new Exception("Erro ao cadastrar!");
 		}
 	}
+	
+	public List<Conta> listar() throws Exception {
+		WebResource resource = client.resource(URL);
+		ClientResponse resp = resource.accept(MediaType.APPLICATION_JSON).get(ClientResponse.class);
+		
+		if(resp.getStatus() != 200) {
+			throw new Exception("Erro ao listar!");
+		}
+		return Arrays.asList(resp.getEntity(Conta[].class));
+	}
+
+	public void atualizar(Conta conta) throws Exception{
+		WebResource resource = client.resource(URL).path(String.valueOf(conta.getId()));
+		ClientResponse resp = resource.type(MediaType.APPLICATION_JSON).put(ClientResponse.class, conta);
+		
+		if (resp.getStatus() != 200) {
+			throw new Exception("Erro ao atualizar");
+		}
+	}
+	
+	public void remover(int codigo) throws Exception{
+		WebResource resource = client.resource(URL).path(String.valueOf(codigo));
+		ClientResponse resp = resource.delete(ClientResponse.class);
+		
+		if(resp.getStatus() != 204) {
+			throw new Exception("Erro ao remover!");
+		}
+	}
+		
 }
